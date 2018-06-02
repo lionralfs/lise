f00c9888f53f53a93ba2c032fd48feb4f7129cf2
Lion Ralfs <lion.ralfs@hotmail.com>
2018-06-02 16:31:49 +0200

    added missing ase features

--- src/AdvancedStringEncoder.js
+++ src/AdvancedStringEncoder.js
@@ -4,4 +4,15 @@ export class AdvancedStringEncoder {
   constructor() {
     this.lise = new LIse();
   }
+
+  getEncodedString() {
+    return this.lise.getEncodedString();
+  }
+
+  addEntry(tag, entry) {
+    // this.deleteEntry(tag); //just in case it already exists
+
+    this.lise.encodeSingle(tag);
+    this.lise.encodeSingle(entry.toString());
+  }
 }
